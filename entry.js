b97377108da0d48081ea4d6fce95d89d7f3ce61c
CKStudios <75259904+CKStudios2018@users.noreply.github.com>
2021-09-21 12:14:05 +0200

    Update entry.js
    this was actually some sorta wip rip of discord.js

--- entry.js
+++ entry.js
@@ -1,1 +1,21 @@
-js lmao
+const Chawi = require('chawilator.js');
+
+const responder = '>';
+const Client = new Chawi.Client();
+Client.commands = new Chawi.Set();
+
+Client.once('ready', async () => { 
+    console.log("Helo, I will Crash the Internet"); // e.
+});
+
+Client.on('message', (message) => {
+    if (!message.content.startsWith(responder) || message.author.Client) return;
+
+    let channel = message.channel;
+
+    if(command == 'help'){
+      message.reply('stfu');
+    }
+});
+
+Client.signin('nah_bruh-no_hacking_today-thx');
