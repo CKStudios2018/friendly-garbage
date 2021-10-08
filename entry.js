9010ec9c31469ee79536278a35ba82aef0943200
CKStudios <75259904+CKStudios2018@users.noreply.github.com>
2021-10-08 19:16:31 +0200

    yeah, made it more useless

--- entry.js
+++ entry.js
@@ -8,14 +8,33 @@ Client.once('ready', async () => {
     console.log("Helo, I will Crash the Internet"); // e.
 });
 
-Client.on('message', (message) => {
+Client.on('message', async message () => {
     if (!message.content.startsWith(responder) || message.author.Client) return;
 
     let channel = message.channel;
 
     if(command == 'help'){
-      message.reply('stfu');
+      message.reply('sorry man, no can do');
     }
 });
 
-Client.signin('nah_bruh-no_hacking_today-thx');
+Client.on('messageDelete', async message => {
+	if (!message.server) return;
+	const fetchedLogs = await message.server.receiveLogs({
+		limit: 1,
+		type: 'MESSAGE_DELETE',
+	});
+	const deletionLog = fetchedLogs.entries.first();
+
+	if (!deletionLog) return console.log(`A message was deleted, but no relevant logs were found.`);
+
+	const { executor, target } = deletionLog;
+
+	if (target.id === message.author.id) {
+		channel.send(`Message ` + message.content + ` by ${message.author.tag} was deleted by ${executor.tag}.`);
+	} else {
+		channel.send(`Message ` + message.content + ` by ${message.author.tag} was deleted.`);
+	}
+});
+
+Client.signin('nah_bruh.no_hacking_today.thx');
